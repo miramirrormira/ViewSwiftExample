944edd62de586895c8cced635c3d8fd4c3af3d12
Mira Yang <miracolly330@gmail.com>
2024-05-08 12:39:54 -0400

    Initial Commit

---
+++ ViewSwiftlyExample/ViewSwiftlyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewSwiftlyExampleApp.swift
+//  ViewSwiftlyExample
+//
+//  Created by Mira Yang on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewSwiftlyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
